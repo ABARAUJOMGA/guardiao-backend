0b2d4ac062885811e9e1038e6ae4b85c76efd2f3
Lucas <atendimento@abaraujo.com>
2025-12-29 15:33:46 -0300

    fix: inicializa client resend corretamente

--- mailer.js
+++ mailer.js
@@ -1,9 +1,19 @@
+import { Resend } from "resend";
+
+/**
+ * Inicializa o client do Resend
+ */
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+/**
+ * Envio de email
+ */
 export async function enviarEmail({ to, subject, text }) {
   console.log("📨 Enviando email para:", to);
 
   const result = await resend.emails.send({
-    from: "Guardião <onboarding@resend.dev>",
-    to: "jogodemestreoficial@gmail.com",
+    from: "Guardião <onboarding@resend.dev>", // sandbox
+    to,
     subject,
     text
   });
